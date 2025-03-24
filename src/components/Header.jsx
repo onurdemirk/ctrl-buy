df19561846c3fef6c02a468f42ba0301d71aefa7
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2025-03-24 18:00:12 +0100

    implement header with navigation and external links
    - Displays the site title linked to the homepage.
    - Adds external links with icons for RAWG API docs, GitHub repository, and The Odin Project.
    - Incorporates a shopping cart icon that displays the current cart count.

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+
+import { Link } from 'react-router-dom';
+import styles from '../css-modules/header.module.css';
+import { TbApi } from "react-icons/tb";
+import { LiaGithub } from "react-icons/lia";
+import { SiTheodinproject } from "react-icons/si";
+import { PiShoppingCart } from "react-icons/pi";
+
+
+export default function Header({ cartCount }) {
+  return (
+    <div className={styles.header}>
+      <div className={styles.title}>
+        <h3><Link to="/">CTRL+BUY</Link></h3>
+        <div className={styles.logos}>
+          <a href="https://api.rawg.io/docs/" target="_blank" rel="noreferrer">
+            <TbApi size={25} />
+          </a>
+          <a href="https://github.com/onurdemirk/ctrl-buy" target="_blank" rel="noreferrer">
+            <LiaGithub size={25} />
+          </a>
+          <a
+            href="https://www.theodinproject.com/lessons/node-path-react-new-shopping-cart"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <SiTheodinproject size={25} />
+          </a>
+        </div>
+      </div>
+      <div className={styles.cart}>
+        <PiShoppingCart className={styles.cartIcon} />
+        <p>{cartCount}</p>
+      </div>
+    </div>
+  );
+}
