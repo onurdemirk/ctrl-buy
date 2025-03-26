bbae9f67c12e1eb1d599a04309f01d7064891777
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2025-03-26 15:41:32 +0100

    refactor: Update Games component structure
    - Wrap game images in .gameImageContainer for consistent sizing.
    - Adjust NavLink usage for game image and title with state passing.
    - Refine Add to Cart button logic and styling.
    - Tweak platform icon rendering for clarity.

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { NavLink, useOutletContext } from "react-router-dom";
+import { NavLink, useOutletContext,useParams } from "react-router-dom";
 import styles from "../css-modules/games.module.css";
+import { endpoints } from "../../endpoints"
 
 import { FaXbox } from "react-icons/fa";
 import { FaWindows } from "react-icons/fa";
@@ -9,15 +10,22 @@ import { FaApple } from "react-icons/fa";
 import { BsNintendoSwitch } from "react-icons/bs";
 import { FaCheck } from "react-icons/fa";
 
-export default function Games() {
-  const apiKey = import.meta.env.VITE_API_KEY;
 
+export default function Games() {
+  
   const [error, setError] = React.useState(null);
   const [loading, setLoading] = React.useState(true);
 
   const [games, setGames] = React.useState([]);
 
   const { addCart, cartItems } = useOutletContext();
+
+  const { categoryName } = useParams();
+  const selectedCategory = categoryName 
+  ? endpoints.find(e => e.id === categoryName) 
+  : endpoints.find(e => e.id === "main") 
+
+
 
   const getRandomPrice = () => {
     const min = 5;
@@ -29,7 +37,7 @@ export default function Games() {
   };
 
   React.useEffect(() => {
-    fetch(`https://api.rawg.io/api/games?key=${apiKey}&page_size=15`)
+    fetch(selectedCategory.url)
       .then((res) => res.json())
       .then((data) => {
         const gamesWithPrice = data.results.map((game) => ({
@@ -40,14 +48,14 @@ export default function Games() {
       })
       .catch((err) => setError(err))
       .finally(() => setLoading(false));
-  }, []);
+  }, [categoryName]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>A network error was encountered</p>;
 
   return (
     <>
-      <h3 className={styles.gamesTitle}>Games</h3>
+      <h3 className={styles.gamesTitle}>{categoryName ? selectedCategory.text : "Games" }</h3>
       <div className={styles.gameContainer}>
         {games.map((game) => (
             <div key={game.id} className={styles.game}>
@@ -56,7 +64,7 @@ export default function Games() {
             state={{ price: game.price }}
             style={{ textDecoration: "none" }}
           >
-              <div>
+              <div className={styles.gameImageContainer}>
                 <img src={game.background_image} alt={game.slug} />
               </div>
               </NavLink>
